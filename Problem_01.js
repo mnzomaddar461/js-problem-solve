9967f93a69dd1cb81208be982c93aea611aa0c44
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-07-16 12:54:49 +0600

    Problem 01

---
+++ Problem_01.js
@@ -0,0 +1,38 @@
+// Take two variables price1 and price2, and compare them using all comparison operators (==, ===, !=, !==, >, <, >=, <=), printing each result with console.log.
+const price1 = 550;
+const price2 = 530;
+
+if(price1 === price2){
+    console.log('Not Equal!');
+}
+else{
+    console.log('Invalide!')
+}
+
+if(price1 != price2){
+    console.log('True!');
+}
+else{
+    console.log('False!');
+}
+
+if(price1 !== price2){
+    console.log('True!');
+}
+else{
+    console.log('False!');
+}
+
+if(price1 > price2){
+    console.log('True');
+}
+else{
+    console.log('False');
+}
+
+if(price1 <= price2){
+    console.log('True');
+}
+else{
+    console.log('False');
+}
