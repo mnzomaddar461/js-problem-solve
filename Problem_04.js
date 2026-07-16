445fe58d2a5c7a412e713c4af5ddf814ad404c61
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-07-16 16:09:56 +0600

    Problem 04

---
+++ Problem_04.js
@@ -0,0 +1,9 @@
+// Take a variable stock. If stock is 0, print "Out of stock" (use only if, no else).
+let stock = 100;
+stock = 0;
+if(stock === 0){
+    console.log("Out of Stock!");
+}
+else{
+    console.log("In Stock!");
+}
