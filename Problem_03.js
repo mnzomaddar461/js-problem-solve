7af637468d05026989002f481e5556c3c6f78c91
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-07-16 15:56:28 +0600

    Problem 03

---
+++ Problem_03.js
@@ -0,0 +1,16 @@
+//Create a variable isRaining (true/false). If it is true, print "Take an umbrella".
+const isRaining = true;
+
+if(isRaining === false){
+    console.log("No take an umbrella");
+}
+else{
+    console.log("Take an Umberlla");
+}
+
+if(isRaining != false){
+    console.log("No take an umbrella");
+}
+else{
+    console.log("Take an Umberlla");
+}
