869f9e2375d434a1e21d416630135297c35741c7
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-07-17 20:23:25 +0600

    Problem 08

---
+++ Problem_08.js
@@ -0,0 +1,8 @@
+// Using age and hasTicket, print "Entry allowed" if age is above 18 AND the person has a ticket (use &&).
+
+let age = 18;
+const hasTicket = true;
+
+if(age <= 18 && hasTicket === true){
+    console.log("Entry allowed");
+}
