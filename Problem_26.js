bcf5dbf18434fcbebd1848b1b81f57faaf011441
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-07-22 10:36:17 +0600

    Problem 26

---
+++ Problem_26.js
@@ -0,0 +1,22 @@
+const array = ['kamal', 'jamal', 'tomal'];
+console.log(array);
+console.log(array.length);
+console.log(array[0]);
+console.log(array[2]);
+array[1] = "Naim";
+console.log(array);
+array.push('korim');
+console.log(array);
+const removeItem = array.pop();
+console.log(array);
+console.log("Remove Item: ", removeItem);
+array.unshift('rohim');
+console.log(array);
+const removeFrist = array.shift();
+console.log(array);
+console.log("remove frust item: ", removeFrist);
+const hastrue = array.includes('Naim');
+console.log(hastrue);
+const position = array.indexOf('Naim');
+console.log(position);
+
